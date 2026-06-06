3beb9d8d6508f05c72e526248c0cd890b8020944
Veer Madan <veermadanpvt@gmail.com>
2026-06-06 15:38:43 +0530

    V2.0.2: Hardcoded Port 80 and allowed mvk-network.in host

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,12 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5173,
+    port: 80,
   },
   preview: {
     host: true,
-    port: 5173,
-    strictPort: true,
+    port: 80,
+    strictPort: true, // This forces Vite to crash if Port 80 is taken, rather than silently switching to 5173
     allowedHosts: ['mvk-network.in']
   }
 });
